5aa8e54801f0ef9861510e761132083e3e89ccd9
Raaffael <roblesjonathan96@gmail.com>
2022-11-09 18:23:37 -0700

    updates

--- seed.sql
+++ seed.sql
@@ -1,5 +1,17 @@
-CREATE TABLE items(
-    id serial,
+CREATE TABLE student(
+    student_id serial PRIMARY KEY,
+    name VARCHAR(30)
+);
+CREATE TABLE course(
+    course_id serial PRIMARY KEY,
     name VARCHAR(30),
-    price integer
+    cost int
+);
+CREATE TABLE registration(
+    reg_id serial,
+    student_id INT,
+     CONSTRAINT fk_student FOREIGN KEY (student_id) REFERENCES student(student_id),
+    course_id INT,
+    CONSTRAINT fk_course FOREIGN KEY (course_id) REFERENCES course(course_id),
+    date_registered DATE
 );
