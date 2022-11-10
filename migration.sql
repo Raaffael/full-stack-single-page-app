f411f5562c33aff6e3f06bc6513fe5f3e44d5b75
Raaffael <roblesjonathan96@gmail.com>
2022-11-10 12:45:43 -0700

    ERD only

--- migration.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO items (name,price) VALUES ('disc',9);
-INSERT INTO items (name,price) VALUES ('nail clippers',4);
-INSERT INTO items (name,price) VALUES ('spring',3);
-INSERT INTO items (name,price) VALUES ('plastic fork',6);
-INSERT INTO items (name,price) VALUES ('metal spoon',15);
-INSERT INTO items (name,price) VALUES ('socks',1);
